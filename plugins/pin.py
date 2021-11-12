5456290e2a1c7d9dc23903e0368fbcc6d2006f9c
FlavinDaJessica <92895161+FlavinDaJessica@users.noreply.github.com>
2021-11-12 17:46:35 -0300

    Update pin.py

--- plugins/pin.py
+++ plugins/pin.py
@@ -15,15 +15,15 @@ def pin(message):
     user_id = message.from_user.id
     members = bot.get_chat_member(chat_id, user_id)
     if message.chat.type == "private":
-        bot.reply_to(message, "This command is meant to be used in Groups")
+        bot.reply_to(message, "Este comando deve ser usado em grupos")
         return
     if message.reply_to_message == None:
-        bot.reply_to(message, "Reply to a message to be pinned")
+        bot.reply_to(message, "Responder a uma mensagem a ser fixada")
         return
     if members.status == "administrator" or members.status == "creator":
         bot.pin_chat_message(message.chat.id, message.reply_to_message.message_id)
         bot.delete_message(message.chat.id, message.message_id)      #Added delete some command for misuse of that
         return
     if members.status != "administrator":
-        bot.reply_to(message, "You need to be an Admin to pin the message")
+        bot.reply_to(message, "Você precisa ser um administrador para fixar a mensagem")
         return
